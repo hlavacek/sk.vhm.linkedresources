0fa518e2061ee9b86b570926becad27514323b0b
hlavacek <vladimir.hlavacek@gmail.com>
2015-05-04 20:21:39 +0200

    Implemented detection of links via

--- src/sk/vhm/linkedresources/handlers/OpenLinkedResourceHandler.java
+++ src/sk/vhm/linkedresources/handlers/OpenLinkedResourceHandler.java
@@ -1,5 +1,7 @@
 package sk.vhm.linkedresources.handlers;
 
+import static java.util.Objects.requireNonNull;
+
 import java.net.URI;
 import java.net.URISyntaxException;
 
@@ -12,8 +14,9 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.ITextSelection;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IEditorSite;
@@ -23,23 +26,28 @@ import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.texteditor.AbstractTextEditor;
+import org.eclipse.ui.texteditor.IDocumentProvider;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 import sk.vhm.linkedresources.Activator;
 
 /**
  * Our sample handler extends AbstractHandler, an IHandler base class.
+ * 
  * @see org.eclipse.core.commands.IHandler
  * @see org.eclipse.core.commands.AbstractHandler
  */
 public class OpenLinkedResourceHandler extends AbstractHandler {
-	
+
+	private static final String LINK_DELIMITERS = " :\n\t";
+
 	/**
 	 * the command has been executed, so extract extract the needed information
 	 * from the application context.
 	 */
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		
+
 		IEditorPart editorPart = Activator.getActiveEditor();
 
 		IFile activeFile = (IFile) editorPart.getEditorInput().getAdapter(IFile.class);
@@ -52,59 +60,107 @@ public class OpenLinkedResourceHandler extends AbstractHandler {
 
 		String selectedLink = getSelectedLink(editorPart);
 
-		if (selectedLink == null) {
+		if (selectedLink == null || "".equals(selectedLink)) {
 			MessageDialog.openInformation(window.getShell(), "Select Link", "Please select the link to open.");
 			return null;
 		}
 
 		try {
-			URI linkUri = new URI(null, selectedLink, null);
+			IFile linkedFile = getLinkedFile(currentProject, selectedLink);
 
-			URI resolvedUri = currentProject.getPathVariableManager().resolveURI(linkUri);
-			URI workspaceRelativeUri = ResourcesPlugin.getWorkspace().getRoot().getLocationURI().relativize(resolvedUri);
-			
-			IPath resolvedPath = new Path(workspaceRelativeUri.getPath());
-
-			
-			IFile sampleFile = ResourcesPlugin.getWorkspace().getRoot().getFile(resolvedPath);
-			
-			if (!sampleFile.exists()) {
+			if (!linkedFile.exists()) {
 				MessageDialog.openInformation(window.getShell(), "Invalid Link", "Unable to find the linked file.");
 				return null;
 			}
-			IWorkbenchPage page = window.getActivePage();
-			IDE.openEditor(page, sampleFile, true);
+			openEditor(window, linkedFile);
 
 		} catch (PartInitException e) {
 			Activator.getDefault().log("Failed to open editor for file " + selectedLink, e);
 		} catch (URISyntaxException e) {
 			Activator.getDefault().log("Invalid file URI " + selectedLink, e);
 		}
-		
+
 		return null;
 	}
-	
+
+	private void openEditor(IWorkbenchWindow window, IFile linkedFile) throws PartInitException {
+		IWorkbenchPage page = window.getActivePage();
+		IDE.openEditor(page, linkedFile, true);
+	}
+
+	private IFile getLinkedFile(IProject currentProject, String selectedLink) throws URISyntaxException {
+		URI linkUri = new URI(null, selectedLink, null);
+
+		URI resolvedUri = currentProject.getPathVariableManager().resolveURI(linkUri);
+		URI workspaceRelativeUri = ResourcesPlugin.getWorkspace().getRoot().getLocationURI().relativize(resolvedUri);
+
+		IPath resolvedPath = new Path(workspaceRelativeUri.getPath());
+
+		IFile sampleFile = ResourcesPlugin.getWorkspace().getRoot().getFile(resolvedPath);
+		return sampleFile;
+	}
+
 	private String getSelectedLink(IEditorPart editorPart) {
 		// get editor
-		if (editorPart instanceof AbstractTextEditor) {
-
-			IEditorSite iEditorSite = editorPart.getEditorSite();
-			if (iEditorSite != null) {
-				// get selection provider
-				ISelectionProvider selectionProvider = iEditorSite.getSelectionProvider();
-				if (selectionProvider != null) {
-					ISelection iSelection = selectionProvider.getSelection();
-
-					if (!iSelection.isEmpty()) {
-						return ((ITextSelection) iSelection).getText();
-
-					}
-				}
-			}
-
+		if (!(editorPart instanceof AbstractTextEditor)) {
+			Activator.getDefault().log("The current editor is not a text editor, cannot get selected link.");
+			return null;
 		}
 
+		ITextEditor textEditor = (ITextEditor) editorPart;
+
+		IDocumentProvider provider = requireNonNull(textEditor.getDocumentProvider(), "Failed to get document provider.");
+
+		IEditorSite iEditorSite = requireNonNull(editorPart.getEditorSite(), "Failed to get editor site.");
+
+		ISelectionProvider selectionProvider = requireNonNull(iEditorSite.getSelectionProvider(), "Failed to get selection provider for editor.");
+
+		ITextSelection iSelection = (ITextSelection) selectionProvider.getSelection();
+		
+		String selectedText = iSelection.getText();
+
+		if (!iSelection.isEmpty() && !"".equals(selectedText)) {
+			Activator.getDefault().log("Using selected text for a link: " + selectedText);
+			return selectedText;
+		}
+
+		Activator.getDefault().log("There is no selected text, trying to find a link.");
+		IDocument document = requireNonNull(provider.getDocument(editorPart.getEditorInput()), "Failed to get document for editor.");
+
+		int endOffset = getOffet(document, LINK_DELIMITERS, iSelection.getOffset(), 1);
+		int startOffset = getOffet(document, LINK_DELIMITERS, iSelection.getOffset(), -1) + 1;
+		
+		try {
+			return document.get(startOffset, endOffset - startOffset);
+		} catch (BadLocationException e) {
+			Activator.getDefault().log("Failed to get text from document: " + startOffset + "," + endOffset, e);
+		} 
+		// int offset = textSelection.getOffset();
+		// int lineNumber = document.getLineOfOffset(offset);
+
 		return null;
+	}
+
+	private int getOffet(IDocument document, String linkDelimiters, int startOffset, int increment) {
+
+		int documentLenght = document.getLength();
+		int currentOffset = startOffset;
+		try {
+			while (currentOffset >= 0 && currentOffset < documentLenght) {
+				char currentChar = document.getChar(currentOffset);
+
+				if (LINK_DELIMITERS.indexOf(currentChar) > -1) {
+					return currentOffset;
+				}
+
+				currentOffset += increment;
+			}
+
+		} catch (BadLocationException e) {
+			Activator.getDefault().log("Failed to get character from document: " + currentOffset, e);
+		}
+
+		return currentOffset;
 	}
 
 }
